1054bfaa05a15ddd126e02dc621999cdcf8b7bf0
Dear <siraphat.si@ku.th>
2017-06-09 15:48:22 +0900

    Initial commit

---
+++ MySlackBot.rb
@@ -0,0 +1,60 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'sinatra'
+require 'SlackBot'
+require 'net/http'
+
+class MySlackBot < SlackBot
+  # cool code goes here
+  def delete_say(params, option = {})
+  	message = params[:text].gsub("@Dbot say「",'')
+  	message = message.gsub("」",'')
+  	user_name = params[:user_name] ? "@#{params[:user_name]}" : ""
+  	return {text: "#{user_name} #{message}"}.merge(option).to_json  	
+  end
+  #---------------------------------------------------------------
+  def convert_currency(params, option = {})
+  	message = params[:text].gsub("@Dbot",'')
+  	array = message.split #convert(0) 25(1) thb(2) to(3) jpy(4)
+  	num = array[1].to_f
+  	src = array[2]
+  	dest = array[4]
+
+  	data = Net::HTTP.get('www.google.com', "/finance/converter?a=#{num}&from=#{src}&to=#{dest}")
+    user_name = params[:user_name] ? "@#{params[:user_name]}" : ""
+    if /[\d.]+\s+#{dest}/ =~ data
+      ans = $& 
+      message = "#{user_name} #{num} #{src} = #{ans}"   
+    else
+      message = "ERROR" 
+    end
+    return {text: "#{message} \nsrc : www.google.com/finance/converter?a=#{num}&from=#{src}&to=#{dest}"}.merge(option).to_json 
+  end
+  #----------------------------------------------------------------
+  def help(params, option = {})
+    user_name = params[:user_name] ? "@#{params[:user_name]}" : ""
+    return {text:"#{user_name}\n1. type say「ooo」\n2. type convert <number> <currency ; JPY> to <currency ; USD>\n"}.merge(option).to_json 
+  end
+  #---------------------------------------------------------------
+end
+
+slackbot = MySlackBot.new
+
+set :environment, :production
+
+get '/' do
+  "SlackBot Server"
+end
+
+post '/slack' do
+  content_type :json
+  if /say/ =~ params[:text]#condition
+  	slackbot.delete_say(params, username: "Dbot")
+  elsif /convert/ =~ params[:text]
+  	slackbot.convert_currency(params, username: "Dbot")
+  elsif /help/ =~ params[:text]
+    slackbot.help(params, username: "Dbot")
+  else
+    slackbot.naive_respond(params, username: "Dbot")
+  end
+end
